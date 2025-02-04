fed6dd4abd588f6d2f4b845df003fbf0a26d5301
HarishB99 <e1138766@u.nus.edu>
2025-02-04 12:20:05 +0800

    Create basic layout using AnchorPane

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,51 @@
+import java.util.Objects;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Duke extends Application {
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+    private Image userImage = new Image(
+            Objects.requireNonNull(
+                    this.getClass().getResourceAsStream(
+                            "/images/DaUser.png")));
+    private Image dukeImage = new Image(
+            Objects.requireNonNull(
+                    this.getClass().getResourceAsStream(
+                            "/images/DaDuke.png")));
+
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+        // Setting up required components
+        this.scrollPane = new ScrollPane();
+        this.dialogContainer = new VBox();
+        this.scrollPane.setContent(this.dialogContainer);
 
-        stage.setScene(scene); // Setting the stage to show our scene
+        this.userInput = new TextField();
+        this.sendButton = new Button("Send");
+
+        DialogBox dialogBox = new DialogBox("Hello!", this.userImage);
+        this.dialogContainer.getChildren().addAll(dialogBox);
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(
+                this.scrollPane, this.userInput, this.sendButton);
+        this.scene = new Scene(mainLayout); // Setting the scene to be our Label
+
+        stage.setScene(this.scene); // Setting the stage to show our scene
         stage.show(); // Render the stage
+
+        // More code to be added here later
     }
 }
